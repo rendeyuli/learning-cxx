7579a70884406db315efa8450cfaea6db78a1b69
rendeyuli <lhn0717@outlook.com>
2024-07-23 00:12:17 +0800

    complete 6-10

--- exercises/08_trivial/main.cpp
+++ exercises/08_trivial/main.cpp
@@ -2,14 +2,16 @@
 
 // READ: Trivial type <https://learn.microsoft.com/en-us/cpp/cpp/trivial-standard-layout-and-pod-types?view=msvc-170>
 struct FibonacciCache {
-    unsigned long long cache[16];
+    unsigned long long cache[16]={0,1};
     int cached;
 };
 
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (;; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    int cached=cache.cached;
+    unsigned long long *ptr=cache.cache;
+    for (;cached<=i;++cached) {
+        *(ptr+cached) = *(ptr+cached - 1) + *(ptr+cached - 2);
     }
     return cache.cache[i];
 }
@@ -19,6 +21,7 @@ int main(int argc, char **argv) {
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
     FibonacciCache fib;
+    fib.cached=2;
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
